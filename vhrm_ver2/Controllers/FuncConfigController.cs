032e2c1d96506cd6da064c1679a6189754af5011
kuxidaica <hth0901@gmail.com>
2019-11-22 18:07:11 +0700

    ~~ HIEUHT

---
+++ vhrm_ver2/Controllers/FuncConfigController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace vhrm.Controllers
+{
+    public class FuncConfigController : Controller
+    {
+        // GET: FuncConfig
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}

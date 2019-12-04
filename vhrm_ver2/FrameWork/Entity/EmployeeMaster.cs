b017ec5293a9c227a67b5a04921300ded7333a66
dzung <dungdxcsht@gmail.com>
2019-12-04 15:56:36 +0700

    commit

--- vhrm_ver2/FrameWork/Entity/EmployeeMaster.cs
+++ vhrm_ver2/FrameWork/Entity/EmployeeMaster.cs
@@ -41,7 +41,7 @@ namespace vhrm.FrameWork.Entity
         //[DataType(DataType.Date)]
         //[DisplayFormat(DataFormatString = "dd-MM-yyyy", ApplyFormatInEditMode = true)]
         public DateTime DATEJOIN { get; set; }
-        [Display(Name = "EMP ID CARD No.")]
+        [Display(Name = "UID Hexadecimal")]
         //[RegularExpression("([0-9]+)", ErrorMessage = "Please enter valid Number")]
         public string IDENTITYCARD { get; set; }
         public string IDISSUEDPLACE { get; set; }
